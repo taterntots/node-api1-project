4d3b128e23b3569995197b29a8617fd6af969b91
taterntots <matthew.a.weidner@gmail.com>
2020-01-13 15:08:24 -0500

    got my server up and running on port 8001. console logs port

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // implement your API here
+const express = require('express'); //import the express package
+const server = express(); //creates the server
+
+// middleware
+server.use(express.json()); //needed to parse JSON
+
+//routes or endpoints
+server.get('/api/users', (req, res) => {
+    res.send('Hello from Express');
+})
+
+//port
+const port = 8001;
+server.listen(port, () => {
+    //start watching for connections on the port specified
+    console.log(`**Server running on port: ${port}**`)
+})
