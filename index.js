d1d61509bac5111cafc7156720933829abe3ada0
taterntots <matthew.a.weidner@gmail.com>
2020-01-13 15:24:37 -0500

    successfully made a get request to api/users. error message working

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // implement your API here
 const express = require('express'); //import the express package
+const database = require('./data/db'); //our database library
 const server = express(); //creates the server
 
 // middleware
 server.use(express.json()); //needed to parse JSON
 
 //routes or endpoints
+server.post('/api/users', (req, res) => {
+    const hubData = req.body; //for this to work you need the server.use(express.json()); above
+})
+
 server.get('/api/users', (req, res) => {
-    res.send('Hello from Express');
+    database.find()
+        .then(data => {
+            console.log('data', data);
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ errorMessage: 'The users information could not be retrieved.' });
+        })
 })
 
 //port
